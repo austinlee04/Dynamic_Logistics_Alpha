39dfa6461f3d0596b019f2e8e5f9fdbcdbd566bc
austinlee04 <56777269+austinlee04@users.noreply.github.com>
2021-10-26 21:02:34 +0900

    .

--- Data_manager.py
+++ Data_manager.py
@@ -14,9 +14,10 @@ class DataManager:
         for i in range(1,num+1):
             parcel_code = format(time, '03')+'P'+str(MAX + i)
             dep, arv = random.sample(nodes, 2)
-            self.parcel[parcel_code] = ['G', [dep, '', '', '', arv]]
+            self.parcel[parcel_code] = ['G', 0, 0, [[dep, False], ['',False], ['',False], ['',False], [arv, False]]]
+                                        # [위치, 진행단계, 총 단계, [경로[위치, 완료여부]]
             self.parcel_expect[parcel_code] = [time, dep, arv, ['', 0], ['', 0], ['', 0], 0]
-            self.parcel_log[parcel_code] = [time, [dep, 0, 0], ['',0,0], ['',0,0], ['',0,0], [arv, 0], 0]
+            self.parcel_log[parcel_code] = [[dep, time], ['',0,0], ['',0,0], ['',0,0], [arv, 0], 0]
             # [출발시간, [위치, 도착시간, 출발시간], 비용]
 
     def route_manager(self):
--- Present_Network_Simulation_V0.py
+++ Present_Network_Simulation_V0.py
@@ -12,15 +12,20 @@ time_max = int(input('max time : '))
 
 for time in range(time_max):
     data.sample_maker(env.hub_ground_codes, random.randint(10**4, 10**5), time)
-    for sample in data.parcel:            # sample = [코드, 상태, 진행단계, 완료단계]
-        if sample[0] == 'R':
+    for key in data.parcel.keys():            # sample = [코드, 상태, 진행단계, 완료단계]
+        if data.parcel[key][0] == 'G':
+            if not data.parcel[key][3][0][1]:
+                data.parcel[key][0] = 'R'
+                data.parcel[key][3][0][1] = True
+                data.parcel_log[key][1][1] = time
+        if sample[1] == 'R':
             road.move(sample[0])
             if sample[1] < sample[2]:
                 sample[1] += 1
             else:
                 pass
                 # 허브 과정으로(하차)
-        elif sample[0] == 'H':
+        elif sample[1] == 'H':
             if sample[1] == 'W':
                 pass
                 # 대기중
@@ -30,10 +35,3 @@ for time in range(time_max):
                 else:
                     pass
                     # 상차
-        elif sample[0] == 'G':
-            if sample[3] == sample[4]:
-                pass
-                # 배송 완료
-            else:       # 배송 시작
-                pass
-                # 상차
