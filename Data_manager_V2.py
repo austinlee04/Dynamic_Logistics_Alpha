c66fc421b8a9636b2ca230a8f1792d7c11f4a585
austinlee04 <56777269+austinlee04@users.noreply.github.com>
2021-11-09 14:52:36 +0900

    .

--- Data_manager_V2.py
+++ Data_manager_V2.py
@@ -1,7 +1,7 @@
 import random
 import csv
 from collections import deque
-from Environment_V1 import LogisticNetwork
+from Environment_V2 import LogisticNetwork
 # try to use pandas
 
 env = LogisticNetwork()
@@ -18,12 +18,13 @@ class DataManager:
         for i in range(1, num+1):
             parcel_code = format(time, '03')+'P'+str(MAX + i)
             dep, arv = random.sample(nodes, 2)
-            self.parcel[parcel_code] = ['G', 0, [dep, False], deque([[arv, False]]), [0, 0]]
+            self.parcel[parcel_code] = ['G', 0, [0, 0], 5,
+                                        [[dep, False], ['', False], ['', False], ['', False], [arv, False]]]
             # [3].append left [경유지, 통과여부]
-            # [위치, 도로운송완료시간, [경로[위치, 도착여부]], 도착지, 현재 경로]
-            self.parcel_log[parcel_code] = [[dep, time], [arv, 0], deque(), []]
-            # [2].append [경유지, 도착시간, 출발시간] --> 허브 통과 정보
-            # [3].append [출발지, 도착지, 거리, 구간 통행량] --> 도로 통과 정보
+            # [위치, 도로운송완료시간, [경로 출발지, 경로 도착지], 경유지 개수, [경유지[위치, 도착여부]]]
+            self.parcel_log[parcel_code] = [[[dep, 0, 0], ['', 0, 0], ['', 0, 0], ['', 0, 0], [arv, 0, 0]],
+                                            []]
+            # [허브 통과 정보[경유지, 도착시간, 출발시간], 도로 운송 정보[출발지, 도착지, 거리, 구간 통행량]] --> append 사용
 
     def save_log(self, name):
 
